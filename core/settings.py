fba0a3b1f4ca4d128fd257c3c82bae4877a505bb
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2024-01-22 11:01:06 +0500

    rewrite to

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", os.environ.get("PROTOCOL", "http"))
-SECURE_SSL_REDIRECT = bool(os.environ.get("SECURE_SSL_REDIRECT", 0))
+# SECURE_SSL_REDIRECT = bool(os.environ.get("SECURE_SSL_REDIRECT", 0))
 SESSION_COOKIE_SECURE = bool(os.environ.get("SESSION_COOKIE_SECURE", 0))
 CSRF_COOKIE_SECURE = bool(os.environ.get("CSRF_COOKIE_SECURE", 0))
 
